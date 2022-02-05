62cf8906675c135827da8e12bbe6bfcb0de13597
Harpo Jaeger <harpo.jaeger@gmail.com>
2022-02-04 19:44:09 -0500

    add comments

--- music_sync.sh
+++ music_sync.sh
@@ -2,11 +2,22 @@
 
 # adb forward tcp:1234 tcp:1234
 
+INDIR="/home/harpo/Music/Swinsian/"
+OUTDIR="/home/harpo/Music/m4a/"
+
 # create mirrored directory structure
-find ./in -type d -exec mkdir -p out/{} \;
+cd "$INDIR"
+find . -type d -exec mkdir -p "$OUTDIR/{}" {} \;
 
-# convert files
-find ./in -type f -exec bash -c '
-ffmpeg -i "$0" -n -vn -ar 44100 -ac 2 -b:a 192k "./out/${0%.*}.m4a"
+# convert files to m4a, including ID3 tags
+# (currently doesn't convert album art)
+find . -type f -exec bash -c '
+ffmpeg -i "$1/$0" -n -vn -ar 44100 -ac 2 -b:a 192k "$2/${0%.*}.m4a"
+' {} "$INDIR" "$OUTDIR" \;
 
-' {} \;
+# Bind localhost:4242 to port 8022 on Android (termux sshd default)
+# (https://glow.li/posts/access-termux-via-usb/)
+# adb forward tcp:4242 tcp:8022
+
+# Sync music
+#rsync -ravzhmp --progress "$OUTDIR" mandroid_usb:/sdcard/Music/synced/
