521f474bd34aae7907d0491e9fdd2884109cb2a1
Harpo Jaeger <harpo.jaeger@gmail.com>
2022-02-18 14:48:27 -0500

    sort files before ffmpeg-ing them

--- music_sync.sh
+++ music_sync.sh
@@ -9,9 +9,14 @@ find . -type d -exec mkdir -p "$OUTDIR/{}" {} \;
 
 # convert files to m4a, including ID3 tags
 # (currently doesn't convert album art)
-find . -type f -exec bash -c '
-ffmpeg -i "$1/$0" -n -vn -ar 44100 -ac 2 -b:a 192k "$2/${0%.*}.m4a"
-' {} "$INDIR" "$OUTDIR" \;
+find . -type f -print0 | \
+# sort files alphabetically (not necessary, but makes it easier to track progress
+sort -zn | \
+# execute the following bash script for each file
+xargs -0 -I '{}' \
+bash -c '
+ffmpeg -i "$1$0" -n -vn -ar 44100 -ac 2 -b:a 192k "$2${0%.*}".m4a' \
+"{}" "$INDIR" "$OUTDIR"
 
 # Bind localhost:4242 to port 8022 on Android (termux sshd default)
 # (https://glow.li/posts/access-termux-via-usb/)
